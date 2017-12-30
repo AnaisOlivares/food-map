5d5c31811af9220bc7408fb79faa9a7baa1d2d08
AnaisOlivares <anaisolivareshilario@gmail.com>
2017-12-30 00:13:29 -0500

    añadiendo mouseover

--- js/filter.js
+++ js/filter.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
       $(this).toggle($(this).html().toLowerCase().indexOf(value) > -1);
     });
   });
+
+  $('img').on('mouseover', function() {
+    $(this).animate({'border-radius': '100px'}, 'slow');
+    $(this).animate({'width': '50%'}, 'slow');
+
+    $(this).css({'background': '#db791f'}, 'slow');
+  });
+  $('img').on('mouseout', function() {
+    $(this).animate({'border-radius': '0px'}, 'slow');
+    $(this).css({'background': 'none'}, 'slow');
+    $(this).animate({'width': '40%'}, 'slow');
+  });
 });
